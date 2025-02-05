3ac81e2b665af31e03bdb123e1182594c4630af8
Fausta Akbar Wijaya <faussss1357@gmail.com>
2025-02-05 17:26:05 +0700

    [Update] theme.dart - melakukan penambahan theme dan (delete test folder)

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,10 @@
+import 'package:ecommerce/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:ecommerce/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:ecommerce/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:ecommerce/utils/theme/custom_themes/chip_theme.dart';
 import 'package:ecommerce/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:ecommerce/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:ecommerce/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:ecommerce/utils/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -6,14 +12,19 @@ class TAppTheme {
   TAppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    fontFamily: 'Poppins',
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
-    textTheme: TTextTheme.lightTextTheme,
-    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
-  );
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: TTextTheme.lightTextTheme,
+      elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+      outlinedButtonTheme: TOoutlinedButtonTheme.lightOutlinedButtonTheme,
+      inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
+      chipTheme: TChipTheme.lightChipTheme,
+      appBarTheme: TAppBarTheme.lightAppBarTheme,
+      checkboxTheme: TcheckboxTheme.lightCheckboxTheme,
+      bottomSheetTheme: TBottomSheetTheme.lightButtonSheetTheme);
 
   static ThemeData darkTheme = ThemeData(
       useMaterial3: true,
@@ -22,5 +33,11 @@ class TAppTheme {
       primaryColor: Colors.blue,
       scaffoldBackgroundColor: Colors.black,
       textTheme: TTextTheme.darkTextTheme,
-      elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme);
+      elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+      outlinedButtonTheme: TOoutlinedButtonTheme.darkOutlinedButtonTheme,
+      inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
+      chipTheme: TChipTheme.darkChipTheme,
+      appBarTheme: TAppBarTheme.darkAppBarTheme,
+      checkboxTheme: TcheckboxTheme.darkCheckboxTheme,
+      bottomSheetTheme: TBottomSheetTheme.darkButtonSheetTheme);
 }
