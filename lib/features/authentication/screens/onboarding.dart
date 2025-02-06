0fcf4af8372b52ca18b2f4412647a6af3d951fdd
Fausta Akbar Wijaya <faussss1357@gmail.com>
2025-02-07 06:47:10 +0700

    [Feature] update onBoardingPage

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -12,29 +12,61 @@ class OnBoardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-            children: [
-              Column(
-                children: [
-                  Image(
-                      width: THelperFunctions.screenWidth() * 0.8,
-                      height: THelperFunctions.screenHeight() * 0.6,
-                      image: AssetImage(TImages.onBoradingImage1)),
-                  Text(
-                    TTexts.onBoardingTitle1,
-                    style: Theme.of(context).textTheme.headlineMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: TSizes.spaceBtwItems,
-                  ),
-                  Text(
-                    TTexts.onBoardingSubTitle1,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )
+            children: const [
+              OnBoardingPage(
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+                image: TImages.onBoradingImage1,
+              ),
+              OnBoardingPage(
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+                image: TImages.onBoradingImage2,
+              ),
+              OnBoardingPage(
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+                image: TImages.onBoradingImage3,
+              ),
             ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+              width: THelperFunctions.screenWidth() * 0.8,
+              height: THelperFunctions.screenHeight() * 0.6,
+              image: AssetImage(image)),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: TSizes.spaceBtwItems,
+          ),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
           ),
         ],
       ),
