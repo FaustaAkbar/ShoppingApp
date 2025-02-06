314a52e81307eea288051b678b5f6da1b3cdf6e6
Fausta Akbar Wijaya <faussss1357@gmail.com>
2025-02-07 06:15:07 +0700

    [Feature] create onboarding page

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,4 +2,11 @@ class TImages {
   // app logo
   static const String darkAppLogo = "assets/logos/logoApk.png";
   static const String lightAppLogo = "assets/logos/logoApk.png";
+
+  static const String onBoradingImage1 =
+      "assets/images/on_boarding_images/shipping.png";
+  static const String onBoradingImage2 =
+      "assets/images/on_boarding_images/point.png";
+  static const String onBoradingImage3 =
+      "assets/images/on_boarding_images/bag.png";
 }
